25c87599f777363165449f2580b5a5d368bd4f8f
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-04-05 20:34:50 -0400

    Added the search funtionality to the program, and it is working fine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'restaurants#index'
   resources :restaurants, only: [:index, :show, :new, :create, :edit, :update]
+  get '/search', to: 'restaurants#search', as: 'search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
