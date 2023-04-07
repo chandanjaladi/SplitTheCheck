825b37576b2b98f18261a760686d20590fb0281c
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-04-06 20:55:20 -0400

    Styling is done for the program and it is functioning as expected

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, :location, presence: true
-  validates :will_split, :will_not_split, allow_blank: true, numericality: {greater_than_or_equal_to: 0}
+  validates :will_split, :will_not_split, numericality: {greater_than_or_equal_to: 0}
 
   def self.search(name: nil, location: nil)
     if !name.nil? && !name.empty?
