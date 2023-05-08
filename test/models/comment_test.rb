0925e89791f2b500261dfeaac56c42d2001989a7
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-05-07 20:08:53 -0400

    Done coding for sprint 3

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -9,9 +9,5 @@ class CommentTest < ActiveSupport::TestCase
     @two = comments(:two)
   end
 
-  test "to_s returns comment summary" do
-    assert_equal "on #{Date.today}, chandan@westga.com says: \"I am number one!\"", @one.to_s
-
-    assert_equal "on #{Date.today}, temp@gmail.com says: \"Who do I work for?\"", @two.to_s
-  end
+  
 end
